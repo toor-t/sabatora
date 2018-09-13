bfee30fb1fbe3c6015d8d3d6824a00a978074e0b
Toor TSUTSUI <toor.tutui@gmail.com>
2018-09-13 23:54:45 +0900

    うまくいかなかったので削除。

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 
 import CounterContainer from '../containers/CounterContainer';
-import Example from '../components/Example';
 
 const Application = () => (
     <div>
         Hello World from Electron!
         <CounterContainer />
-        <Example />
     </div>
 );
 
