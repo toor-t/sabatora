10b0103f1df5b9b27b699b3669ee275137e00459
Toor TSUTSUI <toor.tutui@gmail.com>
2018-10-03 16:51:36 +0900

    Material-UI

--- src/components/CreateFormComponent.tsx
+++ src/components/CreateFormComponent.tsx
@@ -4,6 +4,12 @@
 'use strict';
 
 import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import CreateFormDataGridComponent, {
     ICreateFormDataGridComponentProps
 } from './CreateFormDataGridComponent';
@@ -22,9 +28,20 @@ const CreateFormComponent: React.SFC<ICreateFormComponentProps> = props => {
 
     return (
         <div>
-            <h1>{title}</h1>
-            <br />
-            <CreateFormDataGridComponent {...rest} />
+            <AppBar position="static">
+                <Typography variant="title" color="inherit" noWrap={true}>
+                    帳票作成
+                </Typography>
+            </AppBar>
+            <main>
+                <div>
+                    <div className="text-center">
+                        <h1>{title}</h1>
+                    </div>
+                    <br />
+                    <CreateFormDataGridComponent {...rest} />
+                </div>
+            </main>
         </div>
     );
 };
