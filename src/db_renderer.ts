06e2da29d077efa303c0b4ed9c11d5530cc450ee
Toor TSUTSUI <toor.tutui@gmail.com>
2018-10-12 00:04:58 +0900

    メモリリーク修正

--- src/db_renderer.ts
+++ src/db_renderer.ts
@@ -3,24 +3,36 @@
 //
 'use strict';
 import { ipcRenderer } from 'electron';
+import { UpdateAutoCompleteOptions } from './db';
 
-import { DataDocKeys, DataDoc, ConfDoc, UpdateAutoCompleteOptions } from './db';
-
-// AutoCompleteOptions 更新
+// AutoCompleteOptions
 export const updateAutoCompleteOptions = (query: any, projection: string[] = []): Promise<{}> => {
     return new Promise((resolve, reject) => {
-        ipcRenderer.on(UpdateAutoCompleteOptions.Result, (event: any, result: any) => {
+        const resultListener = (event: any, result: any) => {
             // console.log(`updateAutoCompleteOptions-result=${{ ...result }}`);
             resolve(result);
-        });
-        ipcRenderer.on(UpdateAutoCompleteOptions.Reject, (event: any, error: any) => {
-            // console.log(`updateAutoCompleteOptions-reject=${error}`);
-            reject(error);
-        });
-        ipcRenderer.on(UpdateAutoCompleteOptions.Reply, (event: any, reply: any) => {
+
+            // Remove Listner
+            ipcRenderer.removeListener(UpdateAutoCompleteOptions.Result, resultListener);
+            ipcRenderer.removeListener(UpdateAutoCompleteOptions.Reject, rejectListener);
+        };
+        ipcRenderer.once(UpdateAutoCompleteOptions.Result, resultListener);
+        const rejectListener = (event: any, result: any) => {
+            // console.log(`updateAutoCompleteOptions-result=${{ ...result }}`);
+            reject(result);
+
+            // Remove Listner
+            ipcRenderer.removeListener(UpdateAutoCompleteOptions.Reject, rejectListener);
+            ipcRenderer.removeListener(UpdateAutoCompleteOptions.Result, resultListener);
+        };
+        ipcRenderer.once(UpdateAutoCompleteOptions.Reject, rejectListener);
+        const replyListener = (event: any, reply: any) => {
             /* TODO: */
             // console.log(`updateAutoCompleteOptions-reply = ${reply}`);
-        });
+            // Remove Listner
+            ipcRenderer.removeListener(UpdateAutoCompleteOptions.Reply, replyListener);
+        };
+        ipcRenderer.once(UpdateAutoCompleteOptions.Reply, replyListener);
         //
         // console.log('Send updateAutoCompleteOptions-request');
         ipcRenderer.send(UpdateAutoCompleteOptions.Request, [query, projection]);
